8412c30e0f9d21757b854a4a848e9622bf9e711e
HieuThuan250396 <hieuthuan250396@gmail.com>
2021-04-02 09:52:08 +0700

    add auth route

---
+++ routes/auth.route.js
@@ -0,0 +1,13 @@
+let router = require('express').Router();
+const AuthController = require('../controllers/auth.controller');
+const auth = require('../middleware/auth.middleware');
+
+//public
+router.post('/login', AuthController.login);
+router.post('/signup', UserController.signup);
+
+//private
+router.post('/logout', auth, AuthController.logout);
+router.post('/logout-all', auth, AuthController.logoutAll);
+
+module.exports = router;
